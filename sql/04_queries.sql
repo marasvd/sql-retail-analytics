31a769f13906c4fd42491a923ee00c9b3cb089d9
maria saavedra <mariapazsaavedra9@gmail.com>
2026-08-25 14:34:16 -0500

    avance 2 de consultas sql

--- sql/04_queries.sql
+++ sql/04_queries.sql
@@ -31,3 +31,72 @@ ORDER BY producto_con_mas_ingresos DESC
 LIMIT 1;
 
 --¿Cuál es el importe total de ventas?--
+SELECT SUM(importe) AS total_vendido
+FROM pedidos;
+
+--Nivel 3 — Análisis con JOINs--
+
+--¿Qué categoría vende más?--
+SELECT c.nombre AS categoria_mas_vendida, SUM(lp.cantidad * lp.precio_unitario) AS total_vendido
+FROM productos P 
+INNER JOIN categorias c
+ON  p.categoria_id = c.id
+INNER JOIN lineas_pedido lp
+ON lp.producto_id = p.id
+GROUP BY c.nombre
+ORDER BY total_vendido DESC
+LIMIT 1;
+
+--¿Qué clientes han gastado más?-- forma larga
+SELECT c.nombre AS nombre_cliente, SUM(lp.cantidad * lp.precio_unitario) AS total
+FROM clientes C
+INNER JOIN pedidos P
+ON c.id = p.cliente_id
+INNER JOIN lineas_pedido lp ON p.id = lp.pedido_id
+GROUP BY c.nombre
+ORDER BY total DESC
+LIMIT 1;
+-- forma rapida-- sirve solo si no se tuviera en cuenta el estado
+SELECT c.nombre AS nombre_cliente, SUM(p.importe) AS total
+FROM clientes c
+INNER JOIN pedidos P on c.id = P.cliente_id
+GROUP BY c.nombre
+ORDER BY total DESC
+LIMIT 1;
+--para la trazabiliad con sus diferentes estados--
+SELECT c.nombre AS nombre_cliente, SUM(lp.cantidad * lp.precio_unitario) AS total
+FROM clientes C
+INNER JOIN pedidos P
+ON c.id = p.cliente_id
+INNER JOIN lineas_pedido lp ON p.id = lp.pedido_id
+WHERE p.estado = 'completado'
+GROUP BY c.nombre
+ORDER BY total DESC
+LIMIT 1;
+
+--¿Cuál es el ticket promedio por pedido?--
+SELECT AVG(importe) as ticket_promedio
+FROM pedidos;
+
+--Nivel 4 — Análisis temporal--
+--¿Cómo evolucionan las ventas por mes?--
+SELECT strftime(fecha, '%y-%m') AS mes, SUM(importe) AS total_ventas
+FROM pedidos
+GROUP BY mes
+ORDER BY mes DESC;
+--¿Qué mes fue el mejor?--
+SELECT strftime(fecha, '%y-%m') AS mes, SUM(importe) AS total_ventas
+FROM pedidos
+GROUP BY mes
+ORDER BY total_ventas DESC
+LIMIT 1;
+
+--Nivel 5 — Conclusiones de negocio--
+--¿Qué clientes VIP compran más?--
+SELECT c.nombre AS cliente_vip, SUM(p.importe) AS total
+FROM clientes c
+INNER JOIN pedidos p ON c.id = p.cliente_id
+WHERE c.es_vip = TRUE
+GROUP BY cliente_vip
+ORDER BY total DESC
+LIMIT 5;
